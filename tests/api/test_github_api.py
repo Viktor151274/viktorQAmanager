1d9b71629e45e9b8e58800ac8dd5da0035ae7f86
Viktor151274 <viktor151274@gmail.com>
2024-12-05 13:31:48 +0200

    Modified API test

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -7,3 +7,8 @@ def test_user_exists():
     api = Github()
     user = api.get_user_defunkt()
     assert user['login'] == 'defunkt'
+
+@pytest.mark.api
+def test_user_not_exists(github_api):
+    r = github_api.get_user('butenkosergii')
+    assert r['message'] == 'Not Found' 
