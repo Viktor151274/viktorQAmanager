4f82b5f2a1d2a70e397baefec63ced85380129fd
Viktor151274 <viktor151274@gmail.com>
2025-05-20 12:22:59 +0000

    Update file main.py

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ If the user is between 2 and 12 years old, the program should output "You are a 
 If the user is between 13 and 19 years old, the program should output "You are a teenager.".
 If the user is 20 years old or older, the program should output "You are an adult.".
 """
-
+from termcolor import colored
 
 def main():
-    """Find age category."""
-    age = int(input("Enter your age: \n"))
-    if age < 2:
-        print("You are a baby.")
 
+        age = int(input("Enter your age: \n"))
+
+        if age < 2:
+            print("You are a baby.")
+        elif 2 <= age <= 12:
+            print("You are a child.")
+        elif 13 <= age <= 19:
+            print("You are a teenager.")
+        else:
+            print("You are an adult.")
 
 if __name__ == "__main__":
-    print("Age category detector")  # color this caption
+    print(colored("Age category detector", "cyan", attrs=["bold"]))
     main()
